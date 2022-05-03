e8fff278ce27dc1b0ceaec24151501d826d1a8ac
lsynature <lsynature@gmail.com>
2022-05-03 17:10:01 +0900

    splash, onboarding, 회원가입, main 수정

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:provider/provider.dart';
 import 'auth_service.dart';
 import 'dataservice.dart';
 import 'mainpage.dart';
+import 'onboarding.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // main 함수에서 async 사용하기 위함
@@ -21,17 +22,55 @@ void main() async {
   );
 }
 
+class Splash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Icon(
+          Icons.apartment_outlined,
+          size: MediaQuery.of(context).size.width * 0.785,
+        ),
+      ),
+    );
+  }
+}
+
+// class app_start extends StatelessWidget {
+//   const app_start({Key? key}) : super(key: key);
+
+//   // This widget is the root of your application.
+
+//   @override
+//   Widget build(BuildContext context) {
+//     final user = context.read<AuthService>().currentUser();
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: onboarding_page(),
+//       // home: user == null ? LoginPage() : main_page(),
+//     );
+//   }
+// }
+
 class app_start extends StatelessWidget {
   const app_start({Key? key}) : super(key: key);
-
-  // This widget is the root of your application.
 
   @override
   Widget build(BuildContext context) {
     final user = context.read<AuthService>().currentUser();
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: user == null ? LoginPage() : main_page(),
+    return FutureBuilder(
+      future: Future.delayed(Duration(seconds: 3)),
+      builder: (context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(home: Splash());
+        } else {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: onboarding_page(),
+            // home: user == null ? LoginPage() : main_page(),
+          );
+        }
+      },
     );
   }
 }
--- lib/onboarding.dart
+++ lib/onboarding.dart
@@ -2,31 +2,109 @@ import 'package:dsproject1/loginpage.dart';
 import 'package:dsproject1/mainpage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
 
 class onboarding_page extends StatelessWidget {
   const onboarding_page({Key? key}) : super(key: key);
 
-  @override
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false,
+//       home: onboarding_page(),
+//     );
+//   }
+// }
+
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Center(
-      child: GestureDetector(
-        onTap: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => main_page(),
+        body: IntroductionScreen(
+      pages: [
+        //page1
+        PageViewModel(
+            title: "Datastore",
+            body: "우리는 데이터를 간편하게 거래하는 마켓이에요.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.asset('assets/images/1.jpg', fit: BoxFit.cover),
             ),
-          );
-        },
-        child: Container(
-          alignment: Alignment.center,
-          height: 100,
-          width: 100,
-          color: Colors.amber,
-          child: Text("메인페이지"),
-        ),
-      ),
+            decoration: PageDecoration(
+                titleTextStyle: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                bodyTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ))),
+        //page2
+
+        PageViewModel(
+            title: "Datastore",
+            body: "우리는 데이터를 간편하게 거래하는 마켓이에요.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.asset('assets/images/1.jpg', fit: BoxFit.cover),
+            ),
+            decoration: PageDecoration(
+                titleTextStyle: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                bodyTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ))),
+        //page3
+
+        PageViewModel(
+            title: "Datastore",
+            body: "우리는 데이터를 간편하게 거래하는 마켓이에요.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.asset('assets/images/1.jpg', fit: BoxFit.cover),
+            ),
+            decoration: PageDecoration(
+                titleTextStyle: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                bodyTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ))),
+        //page4
+
+        PageViewModel(
+            title: "Datastore",
+            body: "우리는 데이터를 간편하게 거래하는 마켓이에요.",
+            image: Padding(
+              padding: EdgeInsets.all(32),
+              child: Image.asset('assets/images/1.jpg', fit: BoxFit.cover),
+            ),
+            decoration: PageDecoration(
+                titleTextStyle: TextStyle(
+                  color: Colors.blueAccent,
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                bodyTextStyle: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18,
+                ))),
+      ],
+      next: Text("Next", style: TextStyle(fontWeight: FontWeight.w600)),
+      done: Text("Start Now", style: TextStyle(fontWeight: FontWeight.w600)),
+      onDone: () {
+        //When done button is press
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => main_page()),
+        );
+      },
     ));
   }
 }
